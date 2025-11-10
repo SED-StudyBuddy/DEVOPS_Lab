cd0b5c9d568c77d1efcb75839d6416b6ffc4973b
Andi Rodriguez <90042932+Andriguez@users.noreply.github.com>
2025-11-10 17:22:59 +0100

    fix lint issues

--- test/index.test.js
+++ test/index.test.js
@@ -2,10 +2,9 @@ import request from 'supertest'
 import app from '../src/app.js'
 import { describe, it, expect } from 'vitest'
 
-
 describe('Server', () => {
   it('responds on GET /', async () => {
-    const res = await request(app).get('/');
-    expect(res.statusCode).toBe(200);
-  });
-});
+    const res = await request(app).get('/')
+    expect(res.statusCode).toBe(200)
+  })
+})
